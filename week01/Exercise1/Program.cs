27eda3c7db7db2b10236886c47c0cec6393473e7
munyaradzichiondegwa <bchiondegwa@byupathway.edu>
2025-01-12 01:50:57 +0200

    commit new changes

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -2,8 +2,17 @@ using System;
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello World! This is the Exercise1 Project.");
+        //Prompt the user for their first name
+        Console.WriteLine("What is your first name?");
+        string firstName = Console.ReadLine();
+
+        //Prompt the user for their last name
+        Console.WriteLine("What is your last name?");
+        string lastName = Console.ReadLine();
+
+        // Print the user's name in specified format
+        Console.WriteLine($"\nYour name is {lastName}, {firstName} {lastName}.");
     }
 }
