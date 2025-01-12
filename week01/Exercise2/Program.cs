6099f94ed0e8c1c8f12265806cab863e703ec2db
munyaradzichiondegwa <bchiondegwa@byupathway.edu>
2025-01-12 02:33:00 +0200

    Refactor Main method to prompt for user input and display formatted name

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -2,8 +2,84 @@ using System;
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        // Prompt the user to enter their grade percentage
+        Console.WriteLine("Enter your grade percentage.");
+        int percentage = int.Parse(Console.ReadLine());
+
+        // Variables to store letter grade and sign
+        string letter = "";
+        string sign = "";
+
+        // Determine letter grade 
+        if (percentage >= 90)
+        {
+            letter = "A";
+            sign = "+";
+        }
+        else if (percentage >= 80)
+        {
+            letter = "B";
+            sign = "+";
+        }
+        else if (percentage >= 70)
+        {
+            letter = "C";
+            sign = "+";
+        }
+        else if (percentage >= 60)
+        {
+            letter = "D";
+            sign = "+";
+        }
+        else
+        {
+            letter = "F";
+            sign = "";
+        }
+
+        // Determine sign (#Strech challenge)
+
+        // Only apply to A, B, C, D grades
+        if (letter != "F" && letter != "A")
+
+
+        { // Get the last digit of the percentage
+            int lastDigit = percentage % 10;
+
+            if (percentage % 10 >= 7)
+            {
+                sign = "+";
+            }
+            else if (percentage % 10 <= 2)
+            {
+                sign = "-";
+            }
+        }
+
+        // Special case for A grade (no A+)
+
+        if (letter == "A" && sign == "+")
+        {
+            sign = "";
+        }
+
+        // combine letter and sign
+        string grade = letter + sign;
+
+        // Print the grade
+        Console.WriteLine($"Your grade is: {grade}.");
+
+        // Determine if the student passed
+
+        if (percentage >= 70)
+        {
+            Console.WriteLine("Congrats! You passed the course!");
+        }
+        else
+        {
+            Console.WriteLine("Sorry, you did not pass the course. Keep working hard!");
+        }
     }
 }
