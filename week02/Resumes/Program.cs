83889ef5a9c29dd2d39dbe6b832325e5f2a7a1f4
munyaradzichiondegwa <bchiondegwa@byupathway.edu>
2025-01-20 13:25:24 +0200

    New Changes

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -1,5 +1,6 @@
 // Job.cs
-using System;
+using System
+using System.Collections.Generic;
 
 public class Job
 {
@@ -11,5 +12,72 @@ public class Job
     public void Display()
     {
         Console.WriteLine($"{_jobTitle} ({_company}) {_startYear}-{_endYear}");
+    }
+}
+
+// Program.cs
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Job job1 = new Job();
+        job1._jobTitle = "Software Engineer";
+        job1._company = "Microsoft";
+        job1._startYear = 2019;
+        job1._endYear = 2022;
+        job1.Display();
+
+        Job job2 = new Job();
+        job2._jobTitle = "Manager";
+        job2._company = "Apple";
+        job2._startYear = 2022;
+        job2._endYear = 2023;
+        job2.Display();
+    }
+}
+
+// Resume.cs
+
+public class Resume
+{
+    public string _name;
+    public List<Job> _jobs = new List<Job>();
+
+    public void Display()
+    {
+        Console.WriteLine($"Name: {_name}");
+        Console.WriteLine("Jobs:");
+        foreach (Job job in _jobs)
+        {
+            job.Display();
+        }
+    }
+}
+
+// Program.cs
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Job job1 = new Job();
+        job1._jobTitle = "Software Engineer";
+        job1._company = "Microsoft";
+        job1._startYear = 2019;
+        job1._endYear = 2022;
+
+        Job job2 = new Job();
+        job2._jobTitle = "Manager";
+        job2._company = "Apple";
+        job2._startYear = 2022;
+        job2._endYear = 2023;
+
+        Resume resume = new Resume();
+        resume._name = "Allison Rose";
+        resume._jobs.Add(job1);
+        resume._jobs.Add(job2);
+
+        resume.Display();
     }
 }
