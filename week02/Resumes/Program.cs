7820e9a5f2ca9bb6a6dd8d5a9675e5309834667d
munyaradzichiondegwa <bchiondegwa@byupathway.edu>
2025-01-20 13:41:48 +0200

    New Changes

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -1,5 +1,4 @@
 // Job.cs
-using System
 using System.Collections.Generic;
 
 public class Job
@@ -12,28 +11,6 @@ public class Job
     public void Display()
     {
         Console.WriteLine($"{_jobTitle} ({_company}) {_startYear}-{_endYear}");
-    }
-}
-
-// Program.cs
-
-class Program
-{
-    static void Main(string[] args)
-    {
-        Job job1 = new Job();
-        job1._jobTitle = "Software Engineer";
-        job1._company = "Microsoft";
-        job1._startYear = 2019;
-        job1._endYear = 2022;
-        job1.Display();
-
-        Job job2 = new Job();
-        job2._jobTitle = "Manager";
-        job2._company = "Apple";
-        job2._startYear = 2022;
-        job2._endYear = 2023;
-        job2.Display();
     }
 }
 
@@ -62,19 +39,19 @@ class Program
     static void Main(string[] args)
     {
         Job job1 = new Job();
-        job1._jobTitle = "Software Engineer";
-        job1._company = "Microsoft";
+        job1._jobTitle = "Systems Analyst";
+        job1._company = "DandeMutande";
         job1._startYear = 2019;
         job1._endYear = 2022;
 
         Job job2 = new Job();
-        job2._jobTitle = "Manager";
-        job2._company = "Apple";
+        job2._jobTitle = "Software Engineer";
+        job2._company = "Econet Wireless";
         job2._startYear = 2022;
         job2._endYear = 2023;
 
         Resume resume = new Resume();
-        resume._name = "Allison Rose";
+        resume._name = "Munyaradzi Chiondegwa";
         resume._jobs.Add(job1);
         resume._jobs.Add(job2);
 
