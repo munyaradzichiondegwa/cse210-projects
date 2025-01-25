f31a10c79d2a7754b87ef523eec84972ab356c2b
munyaradzichiondegwa <bchiondegwa@byupathway.edu>
2025-01-25 13:17:09 +0200

    commit

---
+++ week03/ScriptureMemorizer/Scripture.cs
@@ -0,0 +1,55 @@
+using System
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Scripture
+{
+    private Reference _reference;
+    private List<Word> _words;
+    private Random _random;
+
+    public Scripture(Reference reference, string text)
+    {
+        _reference = reference;
+        _random = new Random();
+        
+        // Split text into words and create Word objects
+        _words = text.Split(' ')
+                     .Select(word => new Word(word))
+                     .ToList();
+    }
+
+    public void HideRandomWords(int numberToHide)
+    {
+        var unhiddenWords = _words.Where(w => !w.IsHidden()).ToList();
+        
+        int wordsToHide = Math.Min(numberToHide, unhiddenWords.Count);
+        
+        for (int i = 0; i < wordsToHide; i++)
+        {
+            if (unhiddenWords.Count > 0)
+            {
+                int index = _random.Next(unhiddenWords.Count);
+                unhiddenWords[index].Hide();
+                unhiddenWords.RemoveAt(index);
+            }
+        }
+    }
+
+    public string GetDisplayText()
+    {
+        return string.Join(" ", _words.Select(w => w.GetDisplayText()));
+    }
+
+    public bool IsCompletelyHidden()
+    {
+        return _words.All(w => w.IsHidden());
+    }
+
+    public Reference GetReference()
+    {
+        return _reference;
+    }
+}
