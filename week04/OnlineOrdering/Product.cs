0d8bb0be704c9d381cb50820a32262a9d8d270d9
munyaradzichiondegwa <bchiondegwa@byupathway.edu>
2025-02-02 02:55:53 +0200

    Initial Commit

---
+++ week04/OnlineOrdering/Product.cs
@@ -0,0 +1,31 @@
+using System;
+public class Product
+{
+    private string productName;
+    private string productId;
+    private double price;
+    private int quantity;
+
+    public Product(string productName, string productId, double price, int quantity)
+    {
+        this.productName = productName;
+        this.productId = productId;
+        this.price = price;
+        this.quantity = quantity;
+    }
+
+    public double GetTotalCost()
+    {
+        return price * quantity;
+    }
+
+    public string GetName()
+    {
+        return productName;
+    }
+
+    public string GetProductId()
+    {
+        return productId;
+    }
+}
