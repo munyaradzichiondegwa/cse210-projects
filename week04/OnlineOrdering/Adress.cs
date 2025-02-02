9dd095d4d7c1f4f2a42fb35b50490abfc1f596b2
munyaradzichiondegwa <bchiondegwa@byupathway.edu>
2025-02-02 02:03:10 +0200

    Initial Commit

---
+++ week04/OnlineOrdering/Adress.cs
@@ -0,0 +1,26 @@
+using System;
+public class Address
+{
+    private string streetAddress;
+    private string city;
+    private string stateProvince;
+    private string country;
+
+    public Address(string streetAddress, string city, string stateProvince, string country)
+    {
+        this.streetAddress = streetAddress;
+        this.city = city;
+        this.stateProvince = stateProvince;
+        this.country = country;
+    }
+
+    public bool IsInZimbabwe()
+    {
+        return country.ToLower() == "zimbabwe";
+    }
+
+    public string GetFullAddress()
+    {
+        return $"{streetAddress}\n{city}, {stateProvince}\n{country}";
+    }
+}
