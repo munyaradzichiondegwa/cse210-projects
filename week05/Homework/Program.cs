48787d0b63c1bada10839008ea279f91aa2c250d
munyaradzichiondegwa <bchiondegwa@byupathway.edu>
2025-02-09 23:44:14 +0200

    new changes

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -1,2 +1,22 @@
+// Program.cs
 using System;
 
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Assignment assignment = new Assignment("Samuel Bennett", "Multiplication");
+        Console.WriteLine(assignment.GetSummary());
+
+        MathAssignment mathAssignment = new MathAssignment("Roberto Rodriguez", "Fractions", "7.3", "8-19");
+        Console.WriteLine(mathAssignment.GetSummary());
+        Console.WriteLine(mathAssignment.GetHomeworkList());
+
+        WritingAssignment writingAssignment = new WritingAssignment("Mary Waters", "European History", "The Causes of World War II");
+        Console.WriteLine(writingAssignment.GetSummary());
+        Console.WriteLine(writingAssignment.GetWritingInformation());
+
+
+        Console.ReadKey(); // Keep console window open (optional)
+    }
+}
