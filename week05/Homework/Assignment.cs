c40c3fed6c2f17236ed7811443c013e3649e02e3
munyaradzichiondegwa <bchiondegwa@byupathway.edu>
2025-02-09 23:12:27 +0200

    Initial Commit

---
+++ week05/Homework/Assignment.cs
@@ -0,0 +1,27 @@
+// (Base Class)
+public class Assignment
+{
+    private string _studentName;
+    private string _topic;
+
+    public Assignment(string studentName, string topic)
+    {
+        _studentName = studentName;
+        _topic = topic;
+    }
+
+    public string GetSummary()
+    {
+        return $"{_studentName} - {_topic}";
+    }
+
+    // Option 1: Protected access modifier (less restrictive than private)
+    protected string StudentName { get { return _studentName; } }
+
+    // Option 2: Public getter method (alternative to protected access)
+    public string GetStudentName()
+    {
+        return _studentName;
+    }
+
+}
--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -1,9 +1,2 @@
 using System;
 
-class Program
-{
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello World! This is the Homework Project.");
-    }
-}
