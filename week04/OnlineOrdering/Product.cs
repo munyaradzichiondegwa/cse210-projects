081c582e88c89f1dd99dd25e0e86e62ed76b7700
munyaradzichiondegwa <bchiondegwa@byupathway.edu>
2025-02-09 19:57:15 +0200

    New Changes

--- week04/OnlineOrdering/Product.cs
+++ week04/OnlineOrdering/Product.cs
@@ -1,4 +1,29 @@
 using System;
 using System.Collections.Generic;
 public class Product
-{}
+{
+    string id;
+    string name;
+    decimal price;
+    int quantity;
+
+    // Constructor to create product
+    public Product(string id, string name, decimal price, int quantity)
+    {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+    }
+
+    // Calculate total cost of the product
+    public decimal GeTotalCost()
+    {
+        return price * quantity;
+    }
+
+    // get product details
+    public string GetProductId() {return id; }
+    public string GetName() {return name; }
+
+}
