ca68947efbe6be0c2fc84a517776ce629cbf781a
munyaradzichiondegwa <bchiondegwa@byupathway.edu>
2025-02-09 12:30:38 +0200

    new changes

--- week04/YouTubeVideos/Comment.cs
+++ week04/YouTubeVideos/Comment.cs
@@ -5,11 +5,11 @@ using System.ComponentModel.Design;
 public class Comment 
 {
     // Tracking of who made the commnent and what they said
-    private string _commneterName;
+    private string _commenterName;
     private string _commentText;
 
     // When a new commednt is made, this will give a name and the comment
-    public Comment(string commdntName, string _commentText)
+    public Comment(string commenterName, string commentText)
     {
         _commenterName = _commenterName;
         _commentText = _commentText;
