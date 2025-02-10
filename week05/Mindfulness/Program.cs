9b5a8b4c36681e539e73803864752b565147a974
munyaradzichiondegwa <bchiondegwa@byupathway.edu>
2025-02-10 15:43:05 +0200

    new changes

--- week05/Mindfulness/Program.cs
+++ week05/Mindfulness/Program.cs
@@ -4,6 +4,31 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Mindfulness Project.");
+        while (true)
+        {
+            Console.WriteLine("Mindfulness Program");
+            Console.WriteLine("1. Breathing Activity");
+            Console.WriteLine("2. Reflection Activity");
+            Console.WriteLine("3. Listing Activity");
+            Console.WriteLine("4. Exit");
+            Console.Write("Select an activity: ");
+
+            string choice = Console.ReadLine();
+
+            MindfulnessActivity activity = choice switch
+            {
+                "1" => new BreathingActivity(),
+                "2" => new ReflectionActivity(),
+                "3" => new ListingActivity(),
+                "4" => null,
+                _ => null
+            };
+
+            if (activity == null)
+                break;
+
+            activity.RunActivity();
+            Console.WriteLine();
+        }
     }
 }
